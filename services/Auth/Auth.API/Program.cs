c9214404a1d62539a1c658453a6cbadf0f7ed911
Jair Mancilla Molina <jaiman@MacBook-Pro-de-Jair.local>
2026-09-12 15:25:05 -0600

    definitive url changes on CORS origins

--- services/Auth/Auth.API/Program.cs
+++ services/Auth/Auth.API/Program.cs
@@ -47,7 +47,10 @@ builder.Services.AddCors(options =>
                 .WithOrigins("http://192.168.100.53", "http://localhost:4200", "http://localhost:4201", "http://localhost:4202", "http://localhost:4203",
                     "http://bcl8c20hq8435ht1ejz3u0mf.5.78.222.52.sslip.io",
                     "http://p2iw9w364eunl2tyotiihcrx.5.78.222.52.sslip.io",
-                    "http://qptlq1wl33v86u3y09b5xfhp.5.78.222.52.sslip.io")
+                    "http://qptlq1wl33v86u3y09b5xfhp.5.78.222.52.sslip.io",
+                    "https://lavanderiaatucasa.com",
+                    "https://admin.lavanderiaatucasa.com",
+                    "https://reparto.lavanderiaatucasa.com")
                 .AllowAnyHeader()
                 .AllowAnyMethod();
         });
